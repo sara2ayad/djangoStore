514a32cb7b0d362ac4f423a404e3b424b4234440
sara2ayad <sar2.ayad@hotmail.com>
2022-11-01 20:40:22 +0300

    first Commit

--- django_store/settings.py
+++ django_store/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -111,11 +114,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- store/urls.py
+++ store/urls.py
@@ -1,3 +1,6 @@
-urlpatterns = [
+from django.urls import path
+from . import views
 
+urlpatterns = [
+   path('', views.index, name='store.home')
 ]
